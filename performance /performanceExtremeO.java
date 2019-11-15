95bf1fbc88dfcea2be22a991b19b09c9b9a9f9ea
Abiam Velazquez <abiam222@gmail.com>
2019-11-15 03:15:10 -0500

    adding more notes

---
+++ performance /performanceExtremeO.java
@@ -0,0 +1,29 @@
+/*abstract
+
+What matters is at one and the same time, the absolute number of observable
+metrics and how the end users and stakeholders feel about them
+
+JIT - Just in time complier
+JVM - Java Virtual machine
+
+performance is an experimental science
+it achieves a desired result by:
+*Defining the desired outcome
+*Measuring the existing system
+*Determining what is to be done to achieve the requirement
+*Undertaking an improvement exercise
+*Retesting
+*Determining whether the goal has been achieved
+
+Loc 260 definitions 
+*Throughput
+*Latency
+*Capacity
+*Utilization
+*Efficiency
+*Scaliability
+*Degradation
+
+
+
+*/
